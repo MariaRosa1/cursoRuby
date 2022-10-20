88fe6047df4e151a88ab39ce6c9cf0afca21c396
MariaRosa1 <mariaclararosa38@gmail.com>
2022-10-20 12:08:19 -0300

    Mais alterações

--- modulo_03/crypo_wallet_aula/app/controllers/welcome_controller.rb
+++ modulo_03/crypo_wallet_aula/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby"
+    session[:curso] = 'O dado'
     @meu_nome = params[:nome]
     @idade = params[:idade]
     @curso = 'Rails'
