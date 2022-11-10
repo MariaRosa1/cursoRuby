236f1fe13e8d0a5b10d8135f4959bea1f21d2ee2
MariaRosa1 <mariaclararosa38@gmail.com>
2022-11-10 16:10:10 -0300

    add template de login do user

--- modulo_05/time_to_answer/config/initializers/assets.rb
+++ modulo_05/time_to_answer/config/initializers/assets.rb
@@ -15,10 +15,16 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 #esta dentro de app/assets/
 Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css
                                                   users_backoffice.js users_backoffice.css
-                                                  admin_devise.js admin_devise.css)
+                                                  admin_devise.js admin_devise.css
+                                                  user_devise.js user_devise.css
+                                                  site.js site.css)
 
 #esta dentro de lib/assets/
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css custom.js custom.css img.jpg )
+Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
+                                                  custom.js custom.css
+                                                  img.jpg
+                                                  surface-fix.js surface-fix.css
+                                                  navbar.css)
 
 #esta dentro de vendor/assets/
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js)
--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   namespace :users_backoffice do
     get 'welcome/index'
   end
-  devise_for :users
   namespace :site do
     get 'welcome/index'
+    get 'search', to: 'search#questions'
   end
   namespace :admins_backoffice do
     get 'welcome/index'  #dashboard
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
     resources :questions
     resources :answers
   end
+
   devise_for :admins
+  devise_for :users
 
   get 'users/index'
   get 'users/show'
