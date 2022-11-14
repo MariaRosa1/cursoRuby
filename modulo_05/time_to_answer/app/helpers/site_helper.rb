dbf9d7c4f58027818eeb1d09ebbe8ce7fa9887eb
MariaRosa1 <mariaclararosa38@gmail.com>
2022-11-14 15:30:09 -0300

    Adicionando a verificação se as perguntas estão corretas

--- modulo_05/time_to_answer/app/helpers/site_helper.rb
+++ modulo_05/time_to_answer/app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Últimas perguntas cadastradas"
+    when 'questions'
+      "Resultado para a pesquisa \"#{params[:term]}\""
+    when 'subject'
+      "Mostrando questões para assunto #{params[:subject]}"
+    end
+  end
 end
