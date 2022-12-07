f247662b4fa7f3e44d77bad429fddf8325dee29d
MariaRosa1 <mariaclararosa38@gmail.com>
2022-12-07 15:34:30 -0300

    Começando curso TDD, conhecendo rspec

---
+++ TDDRails/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -0,0 +1,15 @@
+require 'calculator'
+
+describe Calculator do
+  it'use sum method for 2 numbers' do
+    calc = Calculator.new
+    result = calc.sum(5,6)
+    expect(result).to eq(11)
+  end
+
+  example'use sum method for 2 numbers negative' do
+    calc = Calculator.new
+    result = calc.sum(-5,-6)
+    expect(result).to eq(-11)
+  end
+end
