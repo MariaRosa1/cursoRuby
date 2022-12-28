0f3d4a3f3bf38d9e743a9172656e494276d16a6b
MariaRosa1 <mariaclararosa38@gmail.com>
2022-12-28 13:21:57 -0300

    Fixtures e FactoryBot

--- TDDRails/test_app/spec/models/customer_spec.rb
+++ TDDRails/test_app/spec/models/customer_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "#full_name" do
+    customer = create(:customer)
+
+    expect(customer.full_name).to start_with("Sr. ")
+  end
+
+  it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
+
+
+
+  # it "create a customer com factories" do
+  #   customer = create(:customer)
+
+  #   expect(customer.full_name).to eq("Sr. maria rosa")
+  # end
+
+
+  # fixtures :customers
+  # it "create a customer com fixtures" do
+  #   customer = customers(:maria)
+
+  #   expect(customer.full_name).to eq("Sr. Maria Clara")
+  # end
+
+
+
+  # it "create a customer normal" do
+  #   subject.name = "Jose"
+  #   subject.email = "jose@js.com"
+  #   subject.save
+  #   expect(subject.full_name).to eq("Sr. Jose")
+  # end
 end
