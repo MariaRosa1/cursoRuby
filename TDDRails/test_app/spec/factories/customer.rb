ca6449f7bdb0414dee9f09dba7e9b236420b6189
MariaRosa1 <mariaclararosa38@gmail.com>
2022-12-30 13:34:16 -0300

    herança, traits e atribbutes_for

--- TDDRails/test_app/spec/factories/customer.rb
+++ TDDRails/test_app/spec/factories/customer.rb
@@ -1,7 +1,43 @@
 FactoryBot.define do
-  factory :customer do
+  factory :customer, aliases: [:user] do
+
+    transient do
+      upcased {false}
+    end
+
     name {Faker::Name.name}
-    email { Faker::Internet.email}
+    # email { Faker::Internet.email}
     # email { "maria2@maria.com" }
+
+    sequence(:email, 3) { |n| "meu_email-#{n}@email.com"}
+
+    trait :female do
+      gender {"F"}
+    end
+
+    trait :male do
+      gender {"M"}
+    end
+
+    trait :vip do
+      vip {true}
+      days_to_pay {30}
+    end
+
+    trait :default do
+      vip {false}
+      days_to_pay {15}
+    end
+
+    factory :customer_female, traits: [:female]
+    factory :customer_female_default, traits: [:female, :default]
+    factory :customer_female_vip, traits: [:female, :vip]
+    factory :customer_male, traits: [:male]
+    factory :customer_male_default, traits: [:male, :default]
+    factory :customer_male_vip, traits: [:male, :vip]
+
+    after(:create) do |customer, evaluator|
+      customer.name.upcase! if evaluator.upcased
+    end
   end
 end
--- TDDRails/test_app/spec/models/customer_spec.rb
+++ TDDRails/test_app/spec/models/customer_spec.rb
@@ -1,31 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+####################################################################
+# sequence
 
-  it "#full_name" do
+  it "Email" do
     customer = create(:customer)
-
-    expect(customer.full_name).to start_with("Sr. ")
+    customer1 = create(:customer)
+    puts customer.email
+    puts customer1.email
   end
+  
 
-  it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
+####################################################################
+# traits
 
+  # it "Cliente masculino default" do
+  #   customer = create(:customer_male_default)
+  #   expect(customer.vip).to  be false
+  #   expect(customer.gender).to eq("M")
+  # end
+
+  # it "Cliente feminino vip" do
+  #   customer = create(:customer_female_vip)
+  #   expect(customer.vip).to  be true
+  #   expect(customer.gender).to eq("F")
+  # end
+
+  # it "Cliente feminino" do
+  #   customer = create(:customer_female)
+  #   expect(customer.gender).to eq("F")
+  # end
+
+####################################################################
+
+  # it "Atributo transitorio" do
+  #   customer = create(:customer, upcased: true)
+  #   expect(customer.name.upcase).to eq(customer.name)
+  #   puts customer.name
+  # end
+
+####################################################################
+
+    # it "attributes_for" do
+    #   attrs = attributes_for(:customer)
+    #   puts attrs
+    #   customer = Customer.create(attrs)
+    #   expect(customer.full_name).to start_with("Sr. ")
+    # end
+
+####################################################################
+
+    # it "#default" do
+    #   customer = create(:customer_default) #ou create(:customer)
+    #   expect(customer.vip).to be_falsey
+    # end
+
+    # it "#vip" do
+    #   customer = create(:customer_vip) #ou create(:customer)
+    #   expect(customer.vip).to be true
+    # end
+
+####################################################################
+
+  # it "#full_name com nome específico -- Sobrescrevendo" do
+  #   customer = create(:customer, name: "Maria Clara")
+  #   expect(customer.full_name).to eq("Sr. Maria Clara")
+  # end
+
+
+  # it "#full_name" do
+  #   customer = create(:user) #ou create(:customer)
+  #   expect(customer.full_name).to start_with("Sr. ")
+  # end
+
+  # it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 
 
   # it "create a customer com factories" do
   #   customer = create(:customer)
-
   #   expect(customer.full_name).to eq("Sr. maria rosa")
   # end
 
+####################################################################
 
-  # fixtures :customers
+# fixtures :customers
   # it "create a customer com fixtures" do
   #   customer = customers(:maria)
-
   #   expect(customer.full_name).to eq("Sr. Maria Clara")
   # end
-
 
 
   # it "create a customer normal" do
